e9de1419808d21b0a20b9407590d6a918fcfe41e
JinaButkovic <jinabutkovic@gmail.com>
2022-03-28 13:04:42 -0400

    adding modified files

--- src/models.py
+++ src/models.py
@@ -1,1 +1,22 @@
 # TODO - Create SQLAlchemy DB and Movie model
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# CREATE TABLE movie (
+#     movie_id INT          AUTO_INCREMENT,
+#     title    VARCHAR(255) NOT NULL,
+#     director VARCHAR(255) NOT NULL,
+#     rating   INT NOT      NULL,
+#     PRIMARY KEY (movie_id)
+# );
+
+class Movie(db.Model):  
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    director = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f'Movie({self.movie_id}, {self.title}, {self.director}, {self.rating})'
+    
--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -1,19 +1,29 @@
+from models import Movie
+from models import db
 class MovieRepository:
+    
 
     def get_all_movies(self):
         # TODO get all movies from the DB
+        Movie.query.all()
         return None
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
+        Movie.query.get(movie_id)
         return None
 
     def create_movie(self, title, director, rating):
         # TODO create a new movie in the DB
+        new_movie = Movie(title='West Side Story', director='Steven Spielberg', rating='4.3')
+        db.session.add(new_movie)
+        db.session.commit()
         return None
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
+        #https://docs.sqlalchemy.org/en/14/core/operators.html was used to read and impliment the ilike operator which gets all movie titles matching case insenstivie substring.
+        print(('title').ilike('word'))
         return None
 
 
